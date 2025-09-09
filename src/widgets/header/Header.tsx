0d2566fe55a9d2018975596a72e0f500c6c0a3d9
Sammie <Alphasam123@gmail.com>
2025-09-10 00:45:45 +0100

    add mobile features

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,17 +1,37 @@
+import { Link } from 'react-router-dom'
 import { Logo } from '../../components/logo/Logo'
 import styles from './Header.module.scss'
+import { useState } from 'react'
 
 export function Header() {
+	const [menuOpen, setMenuOpen] = useState(false)
+
+
 	return (
-		<div className={styles.header}>
-			<a className={styles.name} href="/">
-				<Logo />
-				<h1>Imperion</h1>
-			</a>
-			<nav className={styles.nav}>
-				<a className={styles.link} href="/">Home</a>
-				<a className={styles.link} href="/groups">Groups</a>
-			</nav>
-		</div>
+		<>
+			<div className={styles.scrollingSpacer}></div> 
+
+			<div className={styles.header}>
+				<Link className={styles.name} to="/">
+					<Logo />
+					<h1>Imperion</h1>
+				</Link>
+
+				<nav className={styles.navBar}>
+					<button
+						className={styles.menuButton}
+						onClick={() => setMenuOpen((prev) => !prev)}
+						aria-label="Toggle menu"
+					>
+						&#9776;
+					</button>
+					
+					<div className={`${styles.navLinks} ${menuOpen ? styles.open : ""}`}>
+						<Link to={"/"} className={styles.navLink}>Home</Link>
+						<Link to={"/groups"} className={styles.navLink}>Groups</Link>
+					</div>
+				</nav>
+			</div>
+		</>
 	)
 }
